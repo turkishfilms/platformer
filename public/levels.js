5d93191bd941f1c6dd2f43d667f1442d05d8cfaa
Hsing <hsingwu27@gmail.com>
2023-08-01 15:52:29 -0700

    class obstacle

---
+++ public/levels.js
@@ -0,0 +1,14 @@
+class obstacle{
+constructor(/*parameters for object creaution*/){
+//initializing the properties
+
+}
+
+//defining the methods
+//(this) refers to this instance of object
+e()
+
+
+
+
+}

a603e9d3cc18e7c2ba3c0e24429754638c0c99b1
Hsing <hsingwu27@gmail.com>
2023-08-01 16:15:33 -0700

    rectangles

--- public/levels.js
+++ public/levels.js
@@ -1,8 +1,13 @@
 class obstacle{
 constructor(/*parameters for object creaution*/){
 //initializing the properties
-
+rect()
+rect()
+rect()
+rect()
+rect()
 }
+
 
 //defining the methods
 //(this) refers to this instance of object

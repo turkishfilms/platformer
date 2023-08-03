4c8c9cf419bb32122d0ce08b7592a9368f94f6e3
Hsing <hsingwu27@gmail.com>
2023-08-03 16:07:33 -0700

    added class  and poperties

--- public/levels.js
+++ public/levels.js
@@ -1,31 +1,54 @@
 class obstacle{
 constructor(x, y, w, h, color ){
-//initializing the properties
-//x y w h , block 
 this.x = x;
 this.y = y;
 this.w = w; 
 this.h = h; 
 this.color = color;
  }
-//defining the methods
-//(this) refers to this instance of object
- 
-
-
-
-
 }
 class spikes{
-    constructor(x, y, w, h, color ){
+constructor(x, y, point, color ){
 this.x 
 this.y
-this.w
-this.h
-
+this.point = abc
+this.color = color
 
 
 
 
     }
 }
+class dissapear{
+    constructor(x, y, w, h, color, hide ){
+    //initializing the properties
+    //x y w h , block 
+    this.x = x;
+    this.y = y;
+    this.w = w; 
+    this.h = h; 
+    this.color = color;
+    this.hide
+     }
+    //defining the methods
+    //(this) refers to this instance of object
+    
+    
+    
+    
+    
+    }
+    class levels{
+        constructor(){
+        //initializing the properties
+
+
+         }
+        //defining the methods
+        //(this) refers to this instance of object
+        
+        
+        
+        
+        
+        }

1aa7dab5b9428d8cd5111ba8593db01609028d6c
heng <hengwu2011@gmail.com>
2023-08-03 15:54:52 -0700

    addded poroperties to player

--- player.js
+++ player.js
@@ -3,11 +3,29 @@ let y = 100
 let width = 50
 let height = 50
 class player{
-  constructor(){
-  background(125);
+  constructor(color , size, postion , speed){
+ this.color = color
+ this.size = size
+ this.postion = postion
+ this.speed = speed
+  }
+/*
+
+
+
+Player can jump and move left and right 
+Have a any color that the player could choose
+Have a speeed!
+Have size
+Have a x and y 
+`
+
+*/
+}
   let player = Bodies.rectangle(x, y, width, height, {
     isStatic: true
   })
 
-}
-}
+const jump = {
+
+  };

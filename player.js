5d474d1d42b6a9454a376987c2e038266e6dd57d
heng <hengwu2011@gmail.com>
2023-08-04 15:29:59 -0700

    added default props to player

--- player.js
+++ player.js
@@ -1,34 +1,43 @@
-let x = 100
-let y = 100
-let width = 50
-let height = 50
-class player{
-  constructor(color , size, postion , speed){
- this.color = color
- this.size = size
- this.postion = postion
- this.speed = speed
+class player {
+  constructor(playerHander = new PlayerHander() , color = {
+    r: 222,
+    g: 150,
+    b: 35,
+    a: 250
+  }, size = {
+    width: 50,
+    height: 50
+  }, postion = {
+    x: 50,
+    y: 50
+  }, speed = {
+    xSpeed: 50,
+    ySpeed: 50
+  }, lives = 3) {
+    this.color = color
+    this.size = size
+    this.postion = postion
+    this.speed = speed
+    this.lives = lives
+    this.playerHander = playerHander
   }
-/*
+  /*
 
 
 
-Player can jump and move left and right 
-Have a any color that the player could choose
-Have a speeed!
-Have size
-Have a x and y 
-`
+  Player can jump and move left and right 
+  Have a any color that the player could choose
+  Have a speeed!
+  Have size
+  Have a x and y 
+  `
 
-*/
-  
+  */
 
-jump (){
-  // this code will make the y higher then minus by the y how high it jumped
+
+  jump() {
+    // this code will make the y higher then minus by the y how high it jumped 
+
+  }
 
 }
-
-
-}
-
- 

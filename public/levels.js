94a24991c1a5f39b2e0d5ca33e080159427c5b21
Hsing <hsingwu27@gmail.com>
2023-08-04 15:44:54 -0700

    Merge branch 'master' of https://github.com/turkishfilms/platformer into thing

--- public/levels.js
+++ public/levels.js
@@ -1,54 +1,62 @@
-class obstacle{
-constructor(x, y, w, h, color ){
-this.x = x;
-this.y = y;
-this.w = w; 
-this.h = h; 
-this.color = color;
- }
-}
-class spikes{
-constructor(x, y, point, color ){
-this.x 
-this.y
-this.point = abc
-this.color = color
-
-
-
-
+class obstacle {
+    constructor(x, y, w, h, color) {
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.color = color;
     }
 }
-class dissapear{
-    constructor(x, y, w, h, color, hide ){
-    //initializing the properties
-    //x y w h , block 
-    this.x = x;
-    this.y = y;
-    this.w = w; 
-    this.h = h; 
-    this.color = color;
-    this.hide
-     }
+
+let equaltriangle = {
+    length: 40,
+    positionx: 12,
+    positionY: 12,
+    direction: 90,
+}
+
+class spikes {
+    constructor(color, length, positionX, positionY, direction) {
+        this.color = color
+
+        this.length = length
+        this.positionX = positionX
+        this.positionY = positionY
+        this.direction = direction
+
+    }
+
+}
+class dissapear {
+    constructor(x, y, w, h, color, hide) {
+        //initializing the properties
+        //x y w h , block 
+        this.x = x;
+        this.y = y;
+        this.w = w;
+        this.h = h;
+        this.color = color;
+        this.hide
+    }
     //defining the methods
     //(this) refers to this instance of object
-    
-    
-    
-    
-    
-    }
-    class levels{
-        constructor(){
+
+
+
+
+
+}
+class levels {
+    constructor() {
         //initializing the properties
 
 
-         }
-        //defining the methods
-        //(this) refers to this instance of object
-        
-        
-        
-        
-        
-        }
+    }
+    //defining the methods
+    //(this) refers to this instance of object
+
+
+
+
+
+}

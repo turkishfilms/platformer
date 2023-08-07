290e4e560b3bcd94b06da05d347a6c5c32af8953
Reginald Robinson <ddrbnsn@gmail.com>
2023-08-07 15:55:26 -0700

    Added upupdateCurrentLevelObstacles

--- public/static/js/class/handlers/PlayerHandler.js
+++ public/static/js/class/handlers/PlayerHandler.js
@@ -1,27 +1,35 @@
-
 /**
- * playerhandler manages players staate 
- * methods jump, addPlayer, removePlayer, signalplayerlostlife, signalPLayerGainLife, player physics happens here 
- * properties: physics
+ * playerhandler manages players staate
+ * methods jump, addPlayer, removePlayer, signalplayerlostlife, signalPLayerGainLife, player physics happens here
+ * properties: physicsX
  * keyboard handled by game not player handler
- * 
+ *
  */
 
+class PlayerHandler {
+  constructor({ physics = new Physics(), currentLevelobstacles = [] } = {}) {
+    this.players = [];
+    this.physics = physics;
+    this.currentLevelobstacles = currentLevelobstacles;
+  }
 
-class PlayerHandler{
-constructor({physics=new Physics(), currentLevelobstacles = []}={}){
-this.players = []
-this.physics = physics
-this.currentLevelobstacles = this.currentLevelobstacles
-}
+  updateCurrentLevelObstacles(obstacles) {
+    this.currentLevelobstacles = [];
+    this.currentLevelobstacles = obstacles;
+  }
 
-jump = (player)=>{
-    player.speed.y -=this.playerJumpSpeed
-}
+  didPlayerHitLevelBoundary({ position, size }, levelBoundaries) {
+    if (position.x + size.width >= levelBoundaries.RIGHTSIDE) return true; //left side
+    //bottom
+    //top
+  }
 
+  jump(player) {
+    player.speed.y -= this.playerJumpSpeed;
+  }
 
-playerFeelsPhyics
-update(){
+  playerFeelsPhyics;
+  update() {
     //every frame check collisions
-}
+  }
 }

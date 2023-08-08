a8e0f194fd2e5ff7c609dcd0114b4c11dbb47229
Reginald Robinson <ddrbnsn@gmail.com>
2023-08-08 15:20:26 -0700

    Added temporary p5 demo

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,15 +1,18 @@
-import { GameHandler } from "./class/handlers/gameHandler";
-let game;
-
+// import { GameHandler } from "./class/handlers/gameHandler";
+// let game;
+let x =0
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(50);
   const boundaries = { width: windowWidth, height: windowHeight };
-  game = new GameHandler(boundaries);
+  // game = new GameHandler(boundaries);
 }
 function keyPressed() {
   //any commands from user are understood -domp
 }
 function draw() {
-  game.nextFrame();
+  background(30,25,230)
+  ellipse(x,100,50,100)
+  x++
+  // game.nextFrame();
 }

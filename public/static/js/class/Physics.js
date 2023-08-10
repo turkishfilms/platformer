4be0a77b0a9f79878cef5056e74efee3e44317c6
Reginald Robinson <ddrbnsn@gmail.com>
2023-08-10 16:46:52 -0700

    Added physics object
    ---
    gravity amd windspeed props
    ---

---
+++ public/static/js/class/Physics.js
@@ -0,0 +1,6 @@
+class Physics {
+  constructor({ gravity = -9.8, windSpeed = 0 } = {}) {
+    this.gravity = gravity;
+    this.windSpeed = windSpeed;
+  }
+}
--- public/static/js/index.js
+++ public/static/js/index.js
@@ -9,7 +9,25 @@ function setup() {
 }
 function keyPressed() {
   //any commands from user are understood -domp
+if("keyistheleftjey"){
+  game.playerHandler.movePlayer(-5)
 }
+if("keyisright"){ 
+  game.playerHandler.movePlayer(5)
+}
+}
+
+gameFakeVersion ={
+playerHandler : {
+  player : {x:20, y:10},
+  movePlayer : function(howmUchTOmove){
+    this.player.x += howmUchTOmove
+  }
+}
+
+}
+
+
 function draw() {
   background(30, 25, 230);
   ellipse(x, 100, 50, 100);

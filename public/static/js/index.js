cb0eaf69d24cb591cef56c9acfbbbb3fd322312c
Hsing <hsingwu27@gmail.com>
2023-08-14 16:16:24 -0700

    added triangles

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,11 +1,15 @@
-import  GameHandler  from "./class/handlers/gameHandler";
+// import  GameHandler  from "./class/handlers/gameHandler";
+
+const FLOOR_OFFSET= 10
+const FLOOR_DEPTH = 50
+
 let game;
 let x = 0;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(50);
-  const boundaries = { width: windowWidth, height: windowHeight };
-  game = new GameHandler(boundaries);
+  // const boundaries = { width: windowWidth, height: windowHeight };
+  // game = new GameHandler(boundaries);
 }
 function keyPressed() {
   //any commands from user are understood -domp
@@ -17,7 +21,7 @@ if("keyisright"){
 }
 }
 
-gameFakeVersion ={
+let gameFakeVersion ={
 playerHandler : {
   player : {x:20, y:10},
   movePlayer : function(howmUchTOmove){
@@ -27,10 +31,24 @@ playerHandler : {
 
 }
 
-
 function draw() {
-  background(30, 25, 230);
+  background(0);
+  fill(255, 255, 255)
   ellipse(x, 100, 50, 100);
   x++;
-  game.nextFrame();
+  fill(255,0,0)
+  // game.nextFrame();
+  rect(0,windowHeight-FLOOR_OFFSET,windowWidth,FLOOR_DEPTH)
+  rect(10, 200, 150, 50)
+  rect(250, 350, 200, 50)
+  rect(475, 100, 50, 225)
+  rect(75, 475, 50, 25)
+  rect(250, 600, 50, 25)
+  triangle(0, 700, 50, 700, 25, 600)
+for(let i = 0; i<4;i++){
+  triangle(35, 700, 90, 700, 60, 600)
+  triangle(35, 700, 90, 700, 60, 600)
+  triangle(35, 700, 90, 700, 60, 600)
+
+}
 }

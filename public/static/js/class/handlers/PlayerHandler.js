0c8d2524831a57800c84f24c529f74721ab53d1b
heng <hengwu2011@gmail.com>
2023-08-17 15:59:14 -0700

    added a collied funcition makes it so no phase

--- public/static/js/class/handlers/PlayerHandler.js
+++ public/static/js/class/handlers/PlayerHandler.js
@@ -7,7 +7,10 @@
  */
 
 class PlayerHandler {
-  constructor({ physics = new Physics(), currentLevelobstacles = [] } = {}) {
+  constructor({
+    physics = new Physics(),
+    currentLevelobstacles = []
+  } = {}) {
     this.players = [];
     this.physics = physics;
     this.currentLevelobstacles = currentLevelobstacles
@@ -19,30 +22,46 @@ class PlayerHandler {
     this.currentLevelobstacles = obstacles;
   }
 
-  didPlayerHitLevelBoundary({ position, size }, levelBoundaries) {
+  didPlayerHitLevelBoundary({
+    position,
+    size
+  }, levelBoundaries) {
     if (position.x + size.width >= levelBoundaries.RIGHTSIDE) return true; //left side
     //bottom
     //top
   }
 
   jump(player) {
-  // the y will go higher = jump
-  player.potion.y -= this.jumpheight
- 
+    // the y will go higher = jump
+    player.postion.y -= this.jumpheight
+
   }
 
-  playerFeelsPhyics;
   update() {
     //every frame check collisions
+    
   }
-  direction = 50
-moveplayer(player, direction){
-//directioin will be negative when we want to go to the left
-//posiitve if to the right
-player.x += direction
+playercollision(player, obstacles){
+  //code for make the player doesn't phase through walls
+  /**
+   if abouve
+   */
+ if (this.player )
+
+ 
 }
+
+checkCollided(x1,y1,w1,h1,x2,y2,w2,h2){
+  return !(x1 + w1< x2 || y1 + h1 < y2|| x2 + w2< x1 || y2 + h2 < y1)
+
 }
+  gravityForce(player){
+this.player.position
+  }
 
-  
-
-
+  moveplayer(player, direction) {
+    //directioin will be negative when we want to go to the left
+    //posiitve if to the right
+    player.x += direction
+  }
+}

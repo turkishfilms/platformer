c501ff92e318e7fe56bdd2006db208856ab83276
heng <hengwu2011@gmail.com>
2023-08-18 15:53:49 -0700

    make a player collsion and made it check collison

--- public/static/js/class/handlers/PlayerHandler.js
+++ public/static/js/class/handlers/PlayerHandler.js
@@ -52,7 +52,7 @@ playercollision(player, obstacles){
 }
 
 checkCollided(x1,y1,w1,h1,x2,y2,w2,h2){
-  return !(x1 + w1< x2 || y1 + h1 < y2|| x2 + w2< x1 || y2 + h2 < y1)
+  return !(x1 + w1< x2 || y1 + h1 < y2|| x1> x2 + w2 || y2 + h2 < y1)
 
 }
   gravityForce(player){

cdee1a4ea3f7b93e4b9f4f38b1f585c1b38c4f79
Reginald Robinson <ddrbnsn@gmail.com>
2023-12-13 16:35:13 -0800

    Added background color to renderHandler
    -Added show function
    -Added backgroundColor with default in constructor

--- public/static/js/class/handlers/RenderHandler.js
+++ public/static/js/class/handlers/RenderHandler.js
@@ -1,8 +1,28 @@
+
+/**
+ * @description Handles Showing Game Using p5.js
+ * @class RenderHandler
+ * @param {Object} backgroundColor - the background Color
+ *
+ *
+ *
+ * **/
+
+
 class RenderHandler{
-constructor(
-    {}={}){
+    constructor({
+                color = {
+                    red:50,
+                    green:50,
+                    blue:180
+                }
+            }={}){
+        this.backgroundColor = color
+    }
+    show(){
+        const {red,green,blue} = this.backgroundColor
+        background(red,green,blue)
 
-  }
-
+    }
 
 }

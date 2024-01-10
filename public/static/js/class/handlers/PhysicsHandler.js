7bd1a01cc0935fa50c30b7a16f1cf311ee55c8d0
Reginald Robinson <ddrbnsn@gmail.com>
2024-01-10 15:54:12 -0800

    cleaned plan.txt and cleand physics handelr

--- public/static/js/class/handlers/PhysicsHandler.js
+++ public/static/js/class/handlers/PhysicsHandler.js
@@ -1,49 +1,26 @@
-class PhysicsHandler{
-  constructor({engine = Matter.Engine.create(),
-     world = Matter.Composite.create(), 
-     physics = new Physics()}={}){
-    //properties
-    this.engine = engine 
+class PhysicsHandler {
+  constructor({ engine = Matter.Engine.create(),
+    world = Matter.Composite.create(),
+    physics = new Physics()
+  } = {}) {
+    this.engine = engine
     this.world = world
-    this.physics= physics
+    this.physics = physics
   }
 
-  //method
-  simulateWorldByOneFrame(){
-/**use the matter.js engine
- * 
- * 
- * 
- */
-Matter.Engine.update(this.engine)
-
-
-
-
-
+  simulateWorldByOneFrame() {
+    /**use the matter.js engine
+     */
+    Matter.Engine.update(this.engine)
   }
 
-  getPlayerPosition(){console.log("heres player pos")}
-  getObstaclePosition(){}
-  getEnemiePosition(){}
-  getPlatformPosition(){}
-  updatePlayerProperties(){}  
-  updateObstacleProperties(){}  
-  updateEnemyProperties(){}
-  updatePlatformProperties(){} 
+  getPlayerPosition() { console.log("heres player pos") }
+  getObstaclePosition() { }
+  getEnemiePosition() { }
+  getPlatformPosition() { }
+  updatePlayerProperties() { }
+  updateObstacleProperties() { }
+  updateEnemyProperties() { }
+  updatePlatformProperties() { }
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-

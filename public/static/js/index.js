a2eed39fb2eab004e2e24901fc08722ef204497f
Reginald Robinson <ddrbnsn@gmail.com>
2024-01-11 16:13:05 -0800

    Cleaned Unused files

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,22 +1,19 @@
-
-
 let game;
 function setup() {
   createCanvas(windowWidth, windowHeight);
   background(50);
-  game = new GameHandler({levels:levelData})
-  game.setCurrentLevel(2)
+  game = new GameHandler({ levels: levelData })
 }
 
 function keyPressed() {
   //any commands from user are understood -domp
   if (key == "d") {
-    game.movePlayerRight(); 
+    game.movePlayerRight();
   } else if (key == "a") {
     game.movePlayerLeft();
-  }else if (key == "3"){
+  } else if (key == "3") {
     game.setCurrentLevel(game.getPreviousLevel())
-  }else if (key == "4"){
+  } else if (key == "4") {
     game.setCurrentLevel(game.getNextLevel())
   }
 }

a76a39e1017aad4073957711a4064b0395be3aee
Reginald Robinson <ddrbnsn@gmail.com>
2024-01-18 15:32:12 -0800

    Added comment

--- public/static/js/class/handlers/gameHandler.js
+++ public/static/js/class/handlers/gameHandler.js
@@ -1,76 +1,75 @@
 class GameHandler {
-  constructor({
-    levels = [],
-    scoreHandler = new ScoreHandler(),
-    playerHandler = new PlayerHandler(),
-    levelHandler = new LevelHandler(
-      { levels: levels }),
-    physicsHandler = new PhysicsHandler(),
-    renderHandler = { show: () => console.log("rendering") }
+	constructor({
+		levels = [],
+		scoreHandler = new ScoreHandler(),
+		playerHandler = new PlayerHandler(),
+		levelHandler = new LevelHandler(
+			{ levels: levels }),
+		physicsHandler = new PhysicsHandler(),
+		renderHandler = { show: () => console.log("rendering") }
 
-  } = {}) {
-    this.scoreHandler = scoreHandler;
-    this.playerHandler = playerHandler;
-    this.levelHandler = levelHandler;
-    this.physicsHandler = physicsHandler
-    this.renderHandler = renderHandler
-    this.gamePaused = true
-  }
+	} = {}) {
+		this.scoreHandler = scoreHandler;
+		this.playerHandler = playerHandler;
+		this.levelHandler = levelHandler;
+		this.physicsHandler = physicsHandler
+		this.renderHandler = renderHandler
+		this.gamePaused = true
+	}
 
-  gameInit() {
-    this.physicsHandler.addItems(this.playerHandler.players[0], 0) //adding player to physics handler
-    this.physicsHandler.addItems(this.levelHandler.levels[this.getCurrentLevel()], 1) //adding current level obtacles to physics handler
-  }
+	gameInit() {
+		this.physicsHandler.addItems(this.playerHandler.players[0], 0) //adding player to physics handler
+		this.physicsHandler.addItems(this.levelHandler.levels[this.getCurrentLevel()], 1) //adding current level obtacles to physics handler
+	}
 
-  gameStart() {
-    /**
-     * Wipe screen, 
-     * display next level screen
-     *
-      * **/
-    this.physicsHandler.clear(2)//clears obstacles
-    this.physicsHandler.addItems(this.levelHandler.levels[this.getCurrentLevel()])
+	gameStart() {
+		/**
+		 * Wipe screen, 
+		 * display next level screen
+		 *
+		  * **/
+		this.physicsHandler.clear()//clears obstacles
+		this.physicsHandler.addItems(this.levelHandler.levels[this.getCurrentLevel()], 1)
 
-  }
+	}
 
-  nextFrame() {
-    /**
-     * each frame this gets called
-     * sometimes game will be in a level
-     * sometimes it will be on a death screen
-     *
-     * **/
-    this.show()
-    // this.physicsHandler.simulateWorldByOneFrame()
-  }
+	nextFrame() {
+		/**
+		 * each frame this gets called
+		 * sometimes game will be in a level
+		 * sometimes it will be on a death screen
+		 *
+		 * **/
+		this.show()
+		// this.physicsHandler.simulateWorldByOneFrame()
+	}
 
-  show() {
+	show() {
 
-    this.renderHandler.show(this.physicsHandler.getObstaclePosition())
-  }
+		this.renderHandler.show(this.physicsHandler.getObstaclePosition())
+	}
 
-  movePlayerRight() {
-    this.playerHandler.movePlayer(1)
-  }
+	movePlayerRight() {
+		this.playerHandler.movePlayer(1)
+	}
 
-  movePlayerLeft() {
-    this.playerHandler.movePlayer(-1)
-  }
+	movePlayerLeft() {
+		this.playerHandler.movePlayer(-1)
+	}
 
-  getCurrentLevel() {
-    return this.levelHandler.currentLevel
-  }
+	getCurrentLevel() {
+		return this.levelHandler.currentLevel
+	}
 
-  setCurrentLevel(levelNumber) {
-    this.levelHandler.currentLevel = levelNumber
-  }
+	setCurrentLevel(levelNumber) {
+		this.levelHandler.currentLevel = levelNumber
+	}
 
-  getPreviousLevel() {
-    return Math.max(this.levelHandler.currentLevel - 1, 1)
-  }
+	getPreviousLevel() {
+		return Math.max(this.levelHandler.currentLevel - 1, 1) //Ensure previous level always picks a positive levelNumber
+	}
 
-  getNextLevel() {
-    return this.levelHandler.currentLevel + 1
-  }
-
+	getNextLevel() {
+		return this.levelHandler.currentLevel + 1
+	}
 }

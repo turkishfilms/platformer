cb6436a1ef6b6a755c689c4c080b2df221260efb
Reginald Robinson <ddrbnsn@gmail.com>
2024-02-21 14:31:15 -0800

    Added speeds to player

--- public/static/js/class/player.js
+++ public/static/js/class/player.js
@@ -1,32 +1,33 @@
 
 class Player {
-	constructor({ jumpSpeed = {
-		x: 5,
-		y: 5
-	}, color = {
-		r: 222,
-		g: 150,
-		b: 35,
-		a: 250
-	}, size = {
-		width: 50,
-		height: 50
-	}, position = {
-		x: 56,
-		y: 215
-	}, speed = {
-		xSpeed: 50,
-		ySpeed: 50
-	}, lives = 3,
+	constructor({
+		color = {
+			r: 222,
+			g: 150,
+			b: 35,
+			a: 250
+		}, size = {
+			width: 50,
+			height: 50
+		}, position = {
+			x: 56,
+			y: 215
+		}, speed = {
+			xSpeed: 50,
+			ySpeed: 50
+		}, lives = 3,
 		startingJumpCount = 1,
-		maxJumpCount = 3 } = {}) {
+		maxMoveSpeed = 4,
+		maxJumpSpeed = 4,
+		maxJumpCount = 1 } = {}) {
 		this.color = color
 		this.bounds = size
 		this.position = position
 		this.speed = speed
 		this.lives = lives
 
-		this.jumpSpeed = jumpSpeed
+		this.jumpSpeed = maxJumpSpeed
+		this.moveSpeed = maxMoveSpeed
 		this.jumpCount = startingJumpCount
 		this.maxJumpCount = maxJumpCount
 	}

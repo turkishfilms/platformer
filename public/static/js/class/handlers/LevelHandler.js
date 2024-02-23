6e1a5aa78f82df990c2dfb76295902701b69d26c
Reginald Robinson <ddrbnsn@gmail.com>
2024-02-22 17:31:51 -0800

    Added Validity checking

--- public/static/js/class/handlers/LevelHandler.js
+++ public/static/js/class/handlers/LevelHandler.js
@@ -9,8 +9,14 @@ class LevelHandler {
 		return this.levels[levelNumber - 1]
 	}
 
+	/**
+		@param {number} levelNumber - number of level  
+	**/
 	setCurrentLevel(levelNumber) {
-		this.currentLevel = levelNumber
+		if (typeof levelNumber != 'number' || isNaN(levelNumber)) return
+		const minVal = 1
+		const maxVal = this.levels.length - 1
+		this.currentLevel = Math.floor(Math.max(minVal, Math.min(maxVal, levelNumber)))
 	}
 
 }

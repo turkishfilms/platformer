d66d50fff94a0763832d6fd92125e6663a88df6a
Reginald Robinson <ddrbnsn@gmail.com>
2024-03-08 16:10:20 -0800

    refeactored long cod

--- public/static/js/class/handlers/RenderHandler.js
+++ public/static/js/class/handlers/RenderHandler.js
@@ -1,22 +1,14 @@
 class RenderHandler {
-	constructor() {}
+	constructor() { }
 
 	renderFrame() {
 		const physics = game.physicsHandler
 		const pHandler = game.playerHandler
-		const index = game.index
 		background(0);
 		rectMode(CENTER)
-
-		let {
-		x,
-		y,
-		width,
-		height
-	} = pHandler.getPlayerAsOptions()
-	let playerAngle = physics.getPlayerBody().angle
-	if (frameCount %30 < 15) {
-		this.showSprite({
+		let { x, y, width, height } = pHandler.getPlayerAsOptions()
+		let playerAngle = physics.getPlayerBody().angle
+		const data = {
 			color: pHandler.canJump(pHandler.player) ? pHandler.player.color : {
 				r: 255,
 				g: 0,
@@ -31,88 +23,76 @@ class RenderHandler {
 				y: y
 			},
 			angle: playerAngle,
-		}, skull)
-	} else {
-		this.showSprite({
-			color: pHandler.canJump(pHandler.player) ? pHandler.player.color : {
-				r: 255,
-				g: 0,
-				b: 0
-			},
-			size: {
-				w: width,
-				h: height
-			},
-			position: {
-				x: x,
-				y: y
-			},
-			angle: playerAngle,
-		}, cryskull)
+		}
+		const frameCycle = 40
+		if (frameCount % frameCycle < frameCycle / 2) {
+			this.showSprite(data, skull)
+		} else {
+			this.showSprite(data, cryskull)
+		}
+		//this doesnt belong in renderhandler. have small show function in which the data is sent in
+		physics.getObstaclePosition().map(obstacle => this.showRect(obstacle))
+		this.showText(pHandler.player.lives, 80, 80)
 	}
-	//this doesnt belong in renderhandler. have small show function in which the data is sent in
-	physics.getObstaclePosition().map(obstacle => this.showRect(obstacle))
-	this.showText(pHandler.player.lives, 80, 80)
-}
-showRect(data) {
-	const {
-		color: {
-			r,
-			g,
-			b,
-			a
-		} = {
-			r: 255,
-			g: 255,
-			b: 255,
-			a: 255
-		},
-		size: {
-			w,
-			h
-		},
-		position: {
-			x,
-			y
-		},
-		angle = 0,
-	} = data;
-	translate(x, y)
-	rotate(angle)
-	fill(r, g, b, a);
-	rect(0, 0, w, h);
-	resetMatrix()
-}
-showText(userText, x, y) {
-	text(userText, x, y)
-}
-showSprite(data, sprite) {
-	const {
-		color: {
-			r,
-			g,
-			b,
-			a
-		} = {
-			r: 255,
-			g: 255,
-			b: 255,
-			a: 255
-		},
-		size: {
-			w,
-			h
-		},
-		position: {
-			x,
-			y
-		},
-		angle = 0,
-	} = data;
-	translate(x, y)
-	rotate(angle)
-	fill(r, g, b, a);
-	image(sprite, 0, 0, w, h)
-	resetMatrix()
-}
+	showRect(data) {
+		const {
+			color: {
+				r,
+				g,
+				b,
+				a
+			} = {
+				r: 255,
+				g: 255,
+				b: 255,
+				a: 255
+			},
+			size: {
+				w,
+				h
+			},
+			position: {
+				x,
+				y
+			},
+			angle = 0,
+		} = data;
+		translate(x, y)
+		rotate(angle)
+		fill(r, g, b, a);
+		rect(0, 0, w, h);
+		resetMatrix()
+	}
+	showText(userText, x, y) {
+		text(userText, x, y)
+	}
+	showSprite(data, sprite) {
+		const {
+			color: {
+				r,
+				g,
+				b,
+				a
+			} = {
+				r: 255,
+				g: 255,
+				b: 255,
+				a: 255
+			},
+			size: {
+				w,
+				h
+			},
+			position: {
+				x,
+				y
+			},
+			angle = 0,
+		} = data;
+		translate(x, y)
+		rotate(angle)
+		fill(r, g, b, a);
+		image(sprite, 0, 0, w, h)
+		resetMatrix()
+	}
 }

20dfaeae50b8240d53ed2f04a2c19dbb45d26703
Reginald Robinson <ddrbnsn@gmail.com>
2024-03-28 14:13:32 -0700

    Added Object destructuring to conctructor, and sprite

--- public/static/js/level/Obstacle.js
+++ public/static/js/level/Obstacle.js
@@ -1,9 +1,10 @@
 
 class Obstacle {
-	constructor(x = 50, y = 50, w = 10, h = 50, color = { r: 255, g: 255, b: 255 }) {
+	constructor({ x = 50, y = 50, w = 10, h = 50, color = { r: 255, g: 255, b: 255 },sprite="cryskull" }={}) {
 		this.position = { x: x, y: y }
 		this.size = { w: w, h: h }
 		this.color = color;
 		this.disapear = false
+		this.sprite = sprite
 	}
 }

04e89b6bfd49a9f87eb75f0e73f5d8bc86b9f2c0
Reginald Robinson <ddrbnsn@gmail.com>
2024-04-25 15:36:26 -0700

    Added getsprite and animation

--- public/static/js/index.js
+++ public/static/js/index.js
@@ -1,41 +1,69 @@
+/**
+ * Goal: Make the resting sprite not the attacking spider
+ * Ingredients:
+ * Non attacking sprite
+ * Translate:
+ * assets.spiderSheet
+ * assets.spiderSprite
+ * getSpriteFromSheet
+ */
 let game,
   assets = {};
 
 function preload() {
-	assets.img = loadImage('static/assets/Player/Health bar/burgercube.jpg');
-	assets.yellowGuy = loadImage('static/assets/ememy.jpg');
-	assets.blackGuy = loadImage('static/assets/steveBlack.png')
-	assets.skull = loadImage('static/assets/skull.png')
-	assets.cryskull = loadImage('static/assets/skull-orig.png')
-	assets.creeper = loadImage('static/assets/creeper.webp')
-	assets.spiderSheet = loadImage('static/assets/LPC_Spiders/LPC_Spiders/spider05.png')
-	assets.deathScreen1 = loadImage('static/assets/black-screen-of-death.webp')
-	assets.clay = loadImage('static/assets/sbs_-_base_materials_pack_128x128/Base Materials 128x128/Clay/Mat_Clay_Red_06-128x128.png')
-	assets.dark = loadImage("static/assets/sbs_-_base_materials_pack_128x128/Base Materials 128x128/Metal/Mat_Metal_Brushed_03-128x128.png")
-
+  assets.img = loadImage("static/assets/Player/Health bar/burgercube.jpg");
+  assets.yellowGuy = loadImage("static/assets/ememy.jpg");
+  assets.blackGuy = loadImage("static/assets/steveBlack.png");
+  assets.skull = loadImage("static/assets/skull.png");
+  assets.cryskull = loadImage("static/assets/skull-orig.png");
+  assets.creeper = loadImage("static/assets/creeper.webp");
+  assets.spiderSheet = loadImage(
+    "static/assets/LPC_Spiders/LPC_Spiders/spider05.png"
+  );
+  assets.deathScreen1 = loadImage("static/assets/black-screen-of-death.webp");
+  assets.clay = loadImage(
+    "static/assets/sbs_-_base_materials_pack_128x128/Base Materials 128x128/Clay/Mat_Clay_Red_06-128x128.png"
+  );
+  assets.dark = loadImage(
+    "static/assets/sbs_-_base_materials_pack_128x128/Base Materials 128x128/Metal/Mat_Metal_Brushed_03-128x128.png"
+  );
 }
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	assets.spiderSprite = getSpriteFromSheet(assets.spiderSheet)
-	background(0);
-	textSize(25)
-	imageMode(CENTER)
-	game = new GameHandler({ physicsHandler: new PhysicsHandler({ sub: ['player', 'obstacles'] }), levels: levelData, dimensions: { height: windowHeight, width: windowWidth } })
+  createCanvas(windowWidth, windowHeight);
+  assets.spiderAttackLeftSprite = getSpriteFromSheet(
+    assets.spiderSheet,
+    64,
+    10
+  );
+  assets.spiderRestSprite = getSpriteFromSheet(assets.spiderSheet, 1);
+  assets.spiderWalkRightSprite = getSpriteFromSheet(assets.spiderSheet, 5);
+  background(0);
+  textSize(25);
+  imageMode(CENTER);
+  game = new GameHandler({
+    physicsHandler: new PhysicsHandler({ sub: ["player", "obstacles"] }),
+    levels: levelData,
+    dimensions: { height: windowHeight, width: windowWidth },
+  });
 }
 
-function getSpriteFromSheet(sheet) {
-	const spriteList = []
-	let size = 64
-	for (let index = 0; index < 10; index++) {
-		spriteList.push(sheet.get(index * size, 64, size, size))
-	}
-
-
-
-	return spriteList
-
+function getSpriteFromSheet(sheet, x, y, width, height) {
+  return sheet.get(x, y, width, height);
 }
+
+function getAnimationFromSheet(sheet, imagePositions = [], imageSize = {width: 0, height: 0}) {
+  const spriteList = [];
+  imagePositions.forEach((position) => {
+    const x = position.x, 
+      y = position.y,
+      width = imageSize.width,
+      height = imageSize.height;
+    spriteList.push(sheet.get(x, y, width, height));
+  });
+  return spriteList;
+}
+
 function keyPressed() {
   switch (key) {
     case "d" || "D":
@@ -63,5 +91,5 @@ function keyPressed() {
 }
 
 function draw() {
-	game.nextFrame()
+  game.nextFrame();
 }

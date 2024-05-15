73580046be9d905113aaace70dbf666283be51a3
Reginald Robinson <ddrbnsn@gmail.com>
2024-05-15 16:42:44 -0700

    Added enemy and enemyHandler Added position size speed damage health and sprites to enemy, Added enemies and addenemeies and getenemey ddata to handler

--- public/static/js/class/Enemy.js
+++ public/static/js/class/Enemy.js
@@ -3,18 +3,22 @@
 - (Methods) Follow te player (move), Attack
 **/
 class Enemy {
-	constructor({ health = 1, position = { x: 0, y: 0 }, speed = { x: 0, y: 0 }, size = { width: 0, height: 0 }, damage = 0, sprites = [] } = {}) {
-		this.health = health
+	constructor({ health = 1, position = { x: 0, y: 0 }, speed = { x: 0, y: 0 }, size = { width: 0, height: 0 }, damage = 0,
+		sprites = { moving: { left: 0, right: 0 }, attacking: { left: 0, right: 0 }, die: 0, damaged: 0 } } = {}) {
 		this.position = position
-		this.speed = speed
 		this.size = size
+		this.speed = speed
 		this.damage = damage
+		this.health = health
 		this.sprites = sprites
 	}
+
 	move() {
+
 	}
 
 	attack() {
+
 	}
 
 
